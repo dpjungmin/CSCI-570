05a87b5fe0cec6ddbe56d4ed5c605873433166bc
dpjungmin <dpjungmin@gmail.com>
2021-07-19 23:30:31 -0700

    add closest pair of points in 2d problem

---
+++ divide-and-conquer/closest-pair-of-points-in-2d/README.md
@@ -0,0 +1,5 @@
+# Closest Pair of Points in 2D
+
+| Problem                                                   |         Code          |     Algorithm      |
+| --------------------------------------------------------- | :-------------------: | :----------------: |
+| [가장 가까운 두 점](https://www.acmicpc.net/problem/2261) | [C++20](solution.cpp) | Divide and Conquer |
---
+++ divide-and-conquer/closest-pair-of-points-in-2d/solution.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Point {
+    int x, y;
+    int dist(const Point& p) const {
+        return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+       
+    int n; cin >> n;
+    vector<Point> px(n), py(n);
+
+    for (int i = 0; i < n; ++i) {
+        int x, y; cin >> x >> y;
+        px[i] = py[i] = {x, y};
+    }
+    
+    sort(begin(px), end(px), [](auto l, auto r) {
+        return make_pair(l.x, l.y) < make_pair(r.x, r.y);
+    });
+    sort(begin(py), end(py), [](auto l, auto r)  {
+        return make_pair(l.y, l.x) < make_pair(r.y, r.x);
+    });
+
+    function<int(vector<Point>, int, int)> closest;
+    closest = [&](vector<Point> y, int l, int r) {
+        // base case
+        if (r - l + 1 <= 3) {
+            int ret = INT_MAX;
+            for (int i = l; i < r; i++) {
+                for (int j = i + 1; j <= r; j++) {
+                    ret = min(ret, px[i].dist(px[j]));
+                }
+            }
+            return ret;
+        }
+        // divide: O(n)
+        int mid = (l + r) / 2;
+        Point mid_point = px[mid];
+        vector<Point> pyl, pyr;
+        for (Point p : y) {
+            if (p.x < mid_point.x || (p.x == mid_point.x && p.y < mid_point.y)) {
+                pyl.push_back(p);
+            } else {
+                pyr.push_back(p);
+            }
+        }
+        // conquer: O(n)
+        int left = closest(pyl, l, mid);
+        int right = closest(pyr, mid + 1, r);
+        // combine: O(n)
+        int ret = min(left, right); // delta
+        vector<Point> tmp;
+        for (Point p : y) {
+            int dx = p.x - mid_point.x;
+            if (dx * dx < ret) {
+                tmp.push_back(p);
+            }
+        }
+        int sz = tmp.size();
+        for (int i = 0; i < sz - 1; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                int dy = tmp[j].y - tmp[i].y;
+                if (dy * dy >= ret) break;
+                ret = min(ret, tmp[i].dist(tmp[j]));
+            }
+        }
+        return ret;
+    };
+
+    cout << closest(py, 0, n - 1);
+}
